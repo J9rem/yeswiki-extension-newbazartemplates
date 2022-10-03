aca3735b1c0169ee5db3093129aec6021283300b
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2022-10-03 09:57:37 +0200

    refactor(lint): fix files

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2015: true,
+    jquery: true
+  },
+  globals: {
+    wiki: 'writable',
+    Vue: 'readable',
+    ace: 'writable'
+  },
+  extends: [
+    'airbnb-base'
+  ],
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module'
+  },
+  rules: {
+    semi: ['error', 'never'],
+    'max-len': ['error', { code: 110 }],
+    'vars-on-top': 'off',
+    'import/no-unresolved': 'off',
+    'import/extensions': ['error', 'always'],
+    eqeqeq: ['error', 'smart'],
+    'comma-dangle': ['error', 'never'],
+    'object-curly-newline': ['error', { multiline: true }],
+    'func-names': ['error', 'never'],
+    'space-before-function-paren': ['error', 'never'],
+    'lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: true }]
+  }
+}
--- javascripts/extract-files-from-node-modules.js
+++ javascripts/extract-files-from-node-modules.js
@@ -1,6 +1,6 @@
 /*
  * This file is part of the YesWiki Extension newbazartemplates.
- * 
+ *
  * Authors : see README.md file that was distributed with this source code.
  *
  * For the full copyright and license information, please view the LICENSE
@@ -14,54 +14,60 @@
 // Include fs and path module
 
 const fs = require('fs-extra')
-const path = require('path');
-const basePath = path.join(__dirname, '../');
+const path = require('path')
 
-function copySync(src,dest,opts){
-  if (fs.existsSync( src )){
-    fs.copySync(path.join(basePath,src),path.join(basePath,dest),opts);
+const basePath = path.join(__dirname, '../')
+
+function copySync(src, dest, opts) {
+  if (fs.existsSync(src)) {
+    fs.copySync(path.join(basePath, src), path.join(basePath, dest), opts)
   } else {
-    console.log(src+" is not existing !");
+    console.log(`${src} is not existing !`)
   }
 }
 
-function mergeFilesSync(sources,dest){
-  
-  var fullDest = path.join(basePath,dest);
-  if (!fs.existsSync( fullDest )){
-    fs.mkdirSync(path.dirname(fullDest),{recursive:true});
+function mergeFilesSync(sources, dest) {
+  const fullDest = path.join(basePath, dest)
+  if (!fs.existsSync(fullDest)) {
+    fs.mkdirSync(path.dirname(fullDest), { recursive: true })
   } else {
-    fs.unlinkSync(fullDest);
+    fs.unlinkSync(fullDest)
   }
-  sources.forEach( file => {
-    var fullSrc = path.join(basePath,file);
-    if (!fs.existsSync( fullSrc )){
-      console.log(fullSrc+" is not existing !");
+  sources.forEach((file) => {
+    const fullSrc = path.join(basePath, file)
+    if (!fs.existsSync(fullSrc)) {
+      console.log(`${fullSrc} is not existing !`)
     } else {
-      fs.appendFileSync(fullDest,fs.readFileSync(fullSrc));
+      fs.appendFileSync(fullDest, fs.readFileSync(fullSrc))
     }
-  });
+  })
 }
 
 // vis-network
-copySync('node_modules/vis-network/dist/vis-network.min.js','javascripts/vendor/vis-network/vis-network.min.js',{overwrite:true});
+copySync('node_modules/vis-network/dist/vis-network.min.js', 'javascripts/vendor/vis-network/vis-network.min.js', { overwrite: true })
 // images
-copySync('node_modules/vis-network/dist/img/','javascripts/vendor/vis-network/img/',{overwrite:true,
-  filter: function (src,dest) {
-    return (fs.statSync(src).isDirectory() && ["img","network"].includes(path.basename(src))) || path.basename(src).match(/.*\.png$/);
-  }});
+copySync('node_modules/vis-network/dist/img/', 'javascripts/vendor/vis-network/img/', {
+  overwrite: true,
+  filter(src, dest) {
+    return (fs.statSync(src).isDirectory() && ['img', 'network'].includes(path.basename(src))) || path.basename(src).match(/.*\.png$/)
+  }
+})
 // license files
-copySync('node_modules/vis-network/','javascripts/vendor/vis-network/',{overwrite:true,
-  filter: function (src,dest) {
-    return (fs.statSync(src).isDirectory() && path.basename(src) == "vis-network") || ["LICENSE-MIT","LICENSE-APACHE-2.0",'README.md'].includes(path.basename(src));
-  }});
-  
-copySync('node_modules/vis-network/dist/dist/vis-network.min.css','styles/vendor/vis-network/vis-network.min.css',{overwrite:true});
+copySync('node_modules/vis-network/', 'javascripts/vendor/vis-network/', {
+  overwrite: true,
+  filter(src, dest) {
+    return (fs.statSync(src).isDirectory() && path.basename(src) == 'vis-network') || ['LICENSE-MIT', 'LICENSE-APACHE-2.0', 'README.md'].includes(path.basename(src))
+  }
+})
+
+copySync('node_modules/vis-network/dist/dist/vis-network.min.css', 'styles/vendor/vis-network/vis-network.min.css', { overwrite: true })
 // license files
-copySync('node_modules/vis-network/','styles/vendor/vis-network/',{overwrite:true,
-  filter: function (src,dest) {
-    return (fs.statSync(src).isDirectory() && path.basename(src) == "vis-network") || ["LICENSE-MIT","LICENSE-APACHE-2.0",'README.md'].includes(path.basename(src));
-  }});
+copySync('node_modules/vis-network/', 'styles/vendor/vis-network/', {
+  overwrite: true,
+  filter(src, dest) {
+    return (fs.statSync(src).isDirectory() && path.basename(src) == 'vis-network') || ['LICENSE-MIT', 'LICENSE-APACHE-2.0', 'README.md'].includes(path.basename(src))
+  }
+})
 
 // example
 // mergeFilesSync(
@@ -69,4 +75,4 @@ copySync('node_modules/vis-network/','styles/vendor/vis-network/',{overwrite:tru
 //     'node_modules/file1.js',
 //     'node_modules/file2.js',
 //   ],
-//   'javascripts/vendor/output-file.js'); 
+//   'javascripts/vendor/output-file.js');
