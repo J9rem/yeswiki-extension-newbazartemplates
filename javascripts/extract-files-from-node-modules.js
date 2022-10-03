0be66a675538909f7a1cea71cd58c2f233c1da50
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2022-10-03 10:05:23 +0200

    fix(.js): manua lint

--- javascripts/extract-files-from-node-modules.js
+++ javascripts/extract-files-from-node-modules.js
@@ -26,46 +26,58 @@ function copySync(src, dest, opts) {
   }
 }
 
-function mergeFilesSync(sources, dest) {
-  const fullDest = path.join(basePath, dest)
-  if (!fs.existsSync(fullDest)) {
-    fs.mkdirSync(path.dirname(fullDest), { recursive: true })
-  } else {
-    fs.unlinkSync(fullDest)
-  }
-  sources.forEach((file) => {
-    const fullSrc = path.join(basePath, file)
-    if (!fs.existsSync(fullSrc)) {
-      console.log(`${fullSrc} is not existing !`)
-    } else {
-      fs.appendFileSync(fullDest, fs.readFileSync(fullSrc))
-    }
-  })
-}
+// function mergeFilesSync(sources, dest) {
+//   const fullDest = path.join(basePath, dest)
+//   if (!fs.existsSync(fullDest)) {
+//     fs.mkdirSync(path.dirname(fullDest), { recursive: true })
+//   } else {
+//     fs.unlinkSync(fullDest)
+//   }
+//   sources.forEach((file) => {
+//     const fullSrc = path.join(basePath, file)
+//     if (!fs.existsSync(fullSrc)) {
+//       console.log(`${fullSrc} is not existing !`)
+//     } else {
+//       fs.appendFileSync(fullDest, fs.readFileSync(fullSrc))
+//     }
+//   })
+// }
 
 // vis-network
-copySync('node_modules/vis-network/dist/vis-network.min.js', 'javascripts/vendor/vis-network/vis-network.min.js', { overwrite: true })
+copySync(
+  'node_modules/vis-network/dist/vis-network.min.js',
+  'javascripts/vendor/vis-network/vis-network.min.js',
+  { overwrite: true }
+)
 // images
 copySync('node_modules/vis-network/dist/img/', 'javascripts/vendor/vis-network/img/', {
   overwrite: true,
-  filter(src, dest) {
-    return (fs.statSync(src).isDirectory() && ['img', 'network'].includes(path.basename(src))) || path.basename(src).match(/.*\.png$/)
+  filter(src) {
+    return (fs.statSync(src).isDirectory() && ['img', 'network'].includes(path.basename(src)))
+      || path.basename(src).match(/.*\.png$/)
   }
 })
 // license files
 copySync('node_modules/vis-network/', 'javascripts/vendor/vis-network/', {
   overwrite: true,
-  filter(src, dest) {
-    return (fs.statSync(src).isDirectory() && path.basename(src) == 'vis-network') || ['LICENSE-MIT', 'LICENSE-APACHE-2.0', 'README.md'].includes(path.basename(src))
+  // filter(src, dest) {
+  filter(src) {
+    return (fs.statSync(src).isDirectory() && path.basename(src) === 'vis-network')
+      || ['LICENSE-MIT', 'LICENSE-APACHE-2.0', 'README.md'].includes(path.basename(src))
   }
 })
 
-copySync('node_modules/vis-network/dist/dist/vis-network.min.css', 'styles/vendor/vis-network/vis-network.min.css', { overwrite: true })
+copySync(
+  'node_modules/vis-network/dist/dist/vis-network.min.css',
+  'styles/vendor/vis-network/vis-network.min.css',
+  { overwrite: true }
+)
 // license files
 copySync('node_modules/vis-network/', 'styles/vendor/vis-network/', {
   overwrite: true,
-  filter(src, dest) {
-    return (fs.statSync(src).isDirectory() && path.basename(src) == 'vis-network') || ['LICENSE-MIT', 'LICENSE-APACHE-2.0', 'README.md'].includes(path.basename(src))
+  filter(src) {
+    return (fs.statSync(src).isDirectory() && path.basename(src) === 'vis-network')
+      || ['LICENSE-MIT', 'LICENSE-APACHE-2.0', 'README.md'].includes(path.basename(src))
   }
 })
 
